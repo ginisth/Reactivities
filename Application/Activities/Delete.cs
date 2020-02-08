60b404243b7117117ff3bed946ca4351facad6ad
ginisth <ginisth@hotmail.com>
2020-02-08 23:25:32 +0200

    End of section 4,simple crud operations with Mediatr

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                    throw new Exception("Couldn't find activity");
+
+                _context.Remove(activity);
+
+                return await _context.SaveChangesAsync() > 0 ? Unit.Value : throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
